b96392391412da9dd87f81d661aad4f2140c063e
ador <rashedulhaque111@gmail.com>
2022-10-15 13:48:58 +0600

    initial commit

---
+++ src/Components/Sidenav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidenav = () => {
+    return (
+        <div>
+            <h1 className='m-24 text-center text-yellow-800 font-bold text-3xl'>Hello </h1>
+        </div>
+    );
+};
+
+export default Sidenav;
